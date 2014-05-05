95a55876d0269629f05232fe8871f31c9302c276
Anderson Furlane <andersonfurlanemartins@gmail.com>
2014-05-05 16:07:33 -0300

    Task rodando em background

--- app/src/main/java/com/furlane/blogreader/app/MainListActivity.java
+++ app/src/main/java/com/furlane/blogreader/app/MainListActivity.java
@@ -1,31 +1,33 @@
 package com.furlane.blogreader.app;
 
 import android.app.ListActivity;
-import android.content.res.Resources;
-import android.support.v7.app.ActionBarActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
-import android.widget.Toast;
 
-import java.util.List;
+import org.apache.http.util.ExceptionUtils;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainListActivity extends ListActivity {
 
-    protected String[] mAndroidNames = {
-
-    };
+    protected String[] mBlogPostTitles;
+    public static final int NUMBER_OF_POSTS = 20;
+    public static final String TAG = MainListActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_list);
 
-        mAndroidNames = getResources().getStringArray(R.array.android_names);
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mAndroidNames);
-        setListAdapter(adapter);
+        GetBlogPostsTask getBlogPostsTask = new GetBlogPostsTask();
+        getBlogPostsTask.execute();
 
 //        Toast.makeText(this, getString(R.string.no_items), Toast.LENGTH_LONG).show();
     }
@@ -49,6 +51,31 @@ public class MainListActivity extends ListActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private class GetBlogPostsTask extends AsyncTask<Object, Void, String> {
+
+        @Override
+        protected String doInBackground(Object[] objects) {
+            int responseCode = -1;
+
+            try{
+                URL blogFeedUrl = new URL("http://blog.teamtreehouse.com/api/get_recent_summary/?count=" + NUMBER_OF_POSTS);
+                HttpURLConnection connection = (HttpURLConnection) blogFeedUrl.openConnection();
+                connection.connect();
+
+                responseCode = connection.getResponseCode();
+                Log.i(TAG, "Code: " + responseCode);
+            }catch (MalformedURLException e){
+                Log.e(TAG, "Exception caught: ", e);
+            }catch (IOException e){
+                Log.e(TAG, "Exception caught: ", e);
+            }catch (Exception e){
+                Log.e(TAG, "Exception caught: ", e);
+            }
+
+            return "Code: " + responseCode;
+        }
     }
 
 }
